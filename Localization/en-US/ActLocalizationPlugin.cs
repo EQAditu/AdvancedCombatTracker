8b556d7e69a7027cd64f0b76dda4e1818ee1a432
EQAditu <52854036+EQAditu@users.noreply.github.com>
2021-11-28 19:52:11 -0500

    Update ActLocalizationPlugin.cs

--- Localization/en-US/ActLocalizationPlugin.cs
+++ Localization/en-US/ActLocalizationPlugin.cs
@@ -7,7 +7,7 @@ using System.Xml;
 
 [assembly: AssemblyTitle("ActLocalization en-US")]
 [assembly: AssemblyDescription("A sample of an ACT plugin that changes localization strings using XML resources.")]
-[assembly: AssemblyVersion("268.0.0.0")]
+[assembly: AssemblyVersion("274.0.0.0")]
 
 namespace ActLocalization
 {
