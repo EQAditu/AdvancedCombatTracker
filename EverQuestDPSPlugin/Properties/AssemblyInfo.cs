7e7135f3fb257a15f3a2f9c9b63ecd1a571d1aa1
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-10-31 20:37:06 -0400

    Combined plugin class into single file with no partial

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.40")]
+[assembly: AssemblyVersion("0.1.0.41")]
 [assembly: InternalsVisibleTo("EQDPSPluginUnitTests")]
