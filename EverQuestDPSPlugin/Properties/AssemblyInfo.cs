a6cb496d73d4fc716567d194b6fdc3299882b6d9
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-11-02 00:10:28 -0400

    Update AssemblyInfo.cs
    updated version information after 0.1.0.45 was tagged to a 0.1.0.44 build

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.45")]
+[assembly: AssemblyVersion("0.1.0.46")]
 [assembly: InternalsVisibleTo("EQDPSPluginUnitTests")]
